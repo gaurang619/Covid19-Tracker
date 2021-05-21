b3e3ac7bba951481fc0c9eac6da48344a9343c35
gaurang619 <gaurangzadafia@gmail.com>
2021-05-21 15:08:16 -0400

    Covid 19 Tracker

---
+++ build/precache-manifest.77aeb4922b9699089126efeb355be1e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d8ef2d481a4e25e1ffff88cffe618ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "fc9ed844a88c1161fb12",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/js/2.ad8e3933.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.ad8e3933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc9ed844a88c1161fb12",
+    "url": "/static/js/main.96273827.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
